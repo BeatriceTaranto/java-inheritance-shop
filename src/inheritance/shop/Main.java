bcccc3d993eb2df6e593be6427150391124276c8
Beatrice Taranto <beataranto@gmail.com>
2024-10-04 16:05:14 +0200

    initial commit

---
+++ src/inheritance/shop/Main.java
@@ -0,0 +1,8 @@
+package inheritance.shop;
+
+public class Main {
+	public static void main(String[] args) {
+        Smartphone smartphone = new Smartphone();
+        Televisore televisore = new Televisore();
+        Cuffie cuffie = new Cuffie();
+}
